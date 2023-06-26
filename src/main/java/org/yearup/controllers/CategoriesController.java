2bfd9b9f708fbd0012518c7d876519d9a2463e01
Bmorse-coder <bmorse@appdev.yearup.org>
2023-06-26 15:26:47 -0400

    Updated CategoriesController

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,6 +1,7 @@
 package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
@@ -9,6 +10,7 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
+@RestController
 // add the annotations to make this a REST controller
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
@@ -29,6 +31,7 @@ public class CategoriesController
     }
 
     // add the appropriate annotation for a get action
+    @GetMapping("/{id}")
     public Category getById(@PathVariable int id)
     {
         // get the category by id
@@ -40,16 +43,8 @@ public class CategoriesController
     @GetMapping("{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
-        public List<Product> getAllProducts(@RequestParam(name = "productId", required = false) Integer productId,
-            @RequestParam(name = "name", required = false) String name,
-            @RequestParam(name = "price", required = false) BigDecimal price,
-            @RequestParam(name = "categoryId", required = false) Integer categoryId,
-            @RequestParam(name = "description", required = false) String description,
-            @RequestParam(name = "color", required = false) String color,
-            @RequestParam(name = "stock", required = false) String stock,
-            @RequestParam(name = "isFeatured", required = false) boolean isFeatured,
-            @RequestParam(name = "imageUrl", required = false) Integer imageUrl) {
-        return productDao.getById(productId, name, price, categoryId, description, color, categoryId, description, color, stock, isFeatured, imageUrl);
+
+        return productDao.listByCategoryId(categoryId);
         // get a list of product by categoryId
 
     }
@@ -57,16 +52,17 @@ public class CategoriesController
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ADMIN')")
     public Category addCategory(@RequestBody Category category)
         {
         // insert the category
-        return categoryDao.getAllCategories(categoryId);
+        return categoryDao.create(category);
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
         // update the category by id
@@ -77,10 +73,10 @@ public class CategoriesController
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
     @DeleteMapping("/{id}")
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('ADMIN')")
     public void deleteCategory(@PathVariable int id)
     {
         categoryDao.delete(id);
         // delete the category by id
     }
-}}
+}
