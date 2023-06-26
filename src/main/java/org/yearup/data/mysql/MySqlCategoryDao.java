93c2c03e06d69fed63e230fe4d07de5cd2fd8133
Bmorse-coder <bmorse@appdev.yearup.org>
2023-06-26 16:56:20 -0400

    Update MySqlCategoryDao

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -1,28 +1,50 @@
 package org.yearup.data.mysql;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
-{
-    public MySqlCategoryDao(DataSource dataSource)
-    {
+public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
+    private final DataSource dataSource;
+
+    @Autowired
+    public MySqlCategoryDao(DataSource dataSource) {
         super(dataSource);
+        this.dataSource = dataSource;
     }
 
     @Override
-    public List<Category> getAllCategories()
-    {
-        // get all categories
-        return null;
+    public List<Category> getAllCategories() {
+        List<Category> categories = new ArrayList<>();
+        String sql = "SELECT * FROM Catagories";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+
+        while (sql.next()) {
+            int categoryID = sql.getInt("CategoryID");
+            String categoryName = sql.getString("CategoryName");
+            Category category = new Category(categoryID, categoryName);
+            categories.add(category);
+        }
+    } catch(SQLException e)
+            e.printStackTrace();
     }
+    // get all categories
+        return ArrayList<>();
+
 
     @Override
     public Category getById(int categoryId)
