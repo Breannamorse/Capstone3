ecdfcdcd7c5a441500a8727e34b169f8510a8110
Bmorse-coder <bmorse@appdev.yearup.org>
2023-06-26 14:53:35 -0400

    Updated CategoryDAO

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,7 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -26,14 +25,14 @@ public class CategoriesController
     public List<Category> getAll()
     {
         // find and return all categories
-        return null;
+        return categoryDao.getAllCategories();
     }
 
     // add the appropriate annotation for a get action
     public Category getById(@PathVariable int id)
     {
         // get the category by id
-        return null;
+        return categoryDao.getById(id);
     }
 
     // the url to return all products in category 1 would look like this
@@ -41,30 +40,47 @@ public class CategoriesController
     @GetMapping("{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
+        public List<Product> getAllProducts(@RequestParam(name = "productId", required = false) Integer productId,
+            @RequestParam(name = "name", required = false) String name,
+            @RequestParam(name = "price", required = false) BigDecimal price,
+            @RequestParam(name = "categoryId", required = false) Integer categoryId,
+            @RequestParam(name = "description", required = false) String description,
+            @RequestParam(name = "color", required = false) String color,
+            @RequestParam(name = "stock", required = false) String stock,
+            @RequestParam(name = "isFeatured", required = false) boolean isFeatured,
+            @RequestParam(name = "imageUrl", required = false) Integer imageUrl) {
+        return productDao.getById(productId, name, price, categoryId, description, color, categoryId, description, color, stock, isFeatured, imageUrl);
         // get a list of product by categoryId
-        return null;
+
     }
 
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Category addCategory(@RequestBody Category category)
-    {
+        {
         // insert the category
-        return null;
+        return categoryDao.getAllCategories(categoryId);
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @PutMapping("/{id}")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
         // update the category by id
+        categoryDao.update(id, category);
     }
 
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteCategory(@PathVariable int id)
     {
+        categoryDao.delete(id);
         // delete the category by id
     }
-}
+}}
